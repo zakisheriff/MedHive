6f0624dd0dd64357324ed618e5f27dd2f770a6a5
abdul rahman nazar <abdulrahmannazar63@gmail.com>
2026-02-09 14:36:09 +0530

    created the needed server file

---
+++ Patient/Backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors()); 
+app.use(express.json());
+
+// Routes
+const registerRoute = require('./routes/register');
+const loginRoute = require('./routes/login');
+
+// Point the paths to the specific files
+app.use('/register', registerRoute);
+app.use('/login', loginRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server started on port ${PORT}`);
+});
