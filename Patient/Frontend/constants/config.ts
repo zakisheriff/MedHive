bd27f400b5d7ed7412611d178bf2176b79c4b8f2
Zaki Sheriff <zakisheriff57@gmail.com>
2026-02-10 22:04:38 +0530

    connect prescription scanner to azure

--- Patient/Frontend/constants/config.ts
+++ Patient/Frontend/constants/config.ts
@@ -11,7 +11,7 @@ const localhost = debuggerHost?.split(':')[0];
 
 export const BASE_URL = __DEV__
     ? `http://${localhost || '192.168.1.190'}:5001`
-    : 'https://patient.medhive.lk';
+    : 'https://medhive-patient-backend-fjaad9afdkc4hvfx.southeastasia-01.azurewebsites.net';
 
 export const API_ENDPOINTS = {
     EXTRACT: `${BASE_URL}/api/extract`,
