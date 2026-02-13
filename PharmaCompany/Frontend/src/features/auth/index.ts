65c0d237f39bc090c8530b25117bd61156bfe929
Kausian <skausian@gmail.com>
2026-02-13 09:46:03 +0530

    Added new version 2 pharma index.html file

--- PharmaCompany/Frontend/src/features/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { LoginPage } from './LoginPage';
-export { RegistrationFlow } from './RegistrationFlow';
-export { CreateAccountPage } from './CreateAccountPage';
-export { AdminAccountPage } from './AdminAccountPage';
-export { SplashScreen } from './SplashScreen';
